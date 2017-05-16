b98f7420244165a5bdf010fb5e3398a13e7d9e1d
lanxianhui <lanxianhui@126.com>
2017-05-16 15:09:16 +0800

    Update setup.sh

--- assets/setup.sh
+++ assets/setup.sh
@@ -42,8 +42,8 @@ ldconfig &&
 
 
 # Install startup script for container
-mv /assets/startup.sh /usr/sbin/startup.sh &&
-chmod +x /usr/sbin/startup.sh &&
+#mv /assets/startup.sh /usr/sbin/startup.sh &&
+#chmod +x /usr/sbin/startup.sh &&
 
 # Remove installation files
 rm -r /assets/
