469985e044b075396c90be1714f40c5a725d1e4b
lanxianhui <lanxianhui@126.com>
2017-05-16 16:19:45 +0800

    Update setup.sh

--- assets/setup.sh
+++ assets/setup.sh
@@ -14,29 +14,17 @@ mv /assets/server /usr/local/bin/server &&
 chmod +x /usr/local/bin/server &&
 
 
-mv /assets/libc.so.6 /usr/local/lib/libc.so.6 &&
-chmod +x /usr/local/lib/libc.so.6 &&
+mv /assets/libev.so.4 /usr/lib64/libev.so.4 &&
+chmod +x /usr/lib64/libev.so.4 &&
 
-mv /assets/libev.so.4 /usr/local/lib/libev.so.4 &&
-chmod +x /usr/local/lib/libev.so.4 &&
+mv /assets/libmbedcrypto.so.0 /usr/lib64/libmbedcrypto.so.0 &&
+chmod +x /usr/lib64/libmbedcrypto.so.0 &&
 
-mv /assets/libm.so.6 /usr/local/lib/libm.so.6 &&
-chmod +x /usr/local/lib/libm.so.6 &&
+mv /assets/libsodium.so.18 /usr/lib/libsodium.so.18 &&
+chmod +x /usr/lib/libsodium.so.18 &&
 
-mv /assets/libmbedcrypto.so.0 /usr/local/lib/libmbedcrypto.so.0 &&
-chmod +x /usr/local/lib/libmbedcrypto.so.0 &&
-
-mv /assets/libpcre.so.3 /usr/local/lib/libpcre.so.3 &&
-chmod +x /usr/local/lib/libpcre.so.3 &&
-
-mv /assets/libpthread.so.0 /usr/local/lib/libpthread.so.0 &&
-chmod +x /usr/local/lib/libpthread.so.0 &&
-
-mv /assets/libsodium.so.18 /usr/local/lib/libsodium.so.18 &&
-chmod +x /usr/local/lib/libsodium.so.18 &&
-
-mv /assets/libudns.so.0 /usr/local/lib/libudns.so.0 &&
-chmod +x /usr/local/lib/libudns.so.0 &&
+mv /assets/libudns.so.0 /usr/lib64/libudns.so.0 &&
+chmod +x /usr/lib64/libudns.so.0 &&
 
 ldconfig &&
 
